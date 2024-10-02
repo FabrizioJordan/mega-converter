04e3b5d4e00e69cc901f6a65e26f4c076a483cd2
FabricioJordan <fabriorange28@gmail.com>
2024-10-01 21:30:42 -0300

    "Updated various Astro components and Tailwind config: modified file inputs, footer text, and typography; added/removed code blocks in Projects component; and updated font family in Tailwind config."

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
+		extend: {
+		  fontFamily: {
+			'lato': ["Lato", "ui-system"],
+			'lato-light': ["Lato-Light", "ui-system"],
+		  },
+		}
+	  },
 	plugins: [],
 }
